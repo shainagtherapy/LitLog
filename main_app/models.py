2fe00b536c1adbb6f2c87bc8d741609a8cc89e16
Shaina G <shainagtherapy@gmail.com>
2025-09-16 14:31:20 -0700

    Detail changes on Log Class and Auth typos

--- main_app/models.py
+++ main_app/models.py
@@ -2,11 +2,20 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+TYPE = (
+    ('print', 'Print/eBook'),
+    ('comicbook', 'Comicbook'),
+    ('audiobook', 'AudioBook'),
+    ('podcast', 'Podcast'),
+)
+
+
+
 # Create your models here.
 class Log(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
-    type = models.CharField(max_length=250)
+    type = models.CharField(max_length=250, choices=TYPE,)
     status = models.CharField(max_length=250)
     notes = models.TextField(max_length=500)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .models import Log
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
